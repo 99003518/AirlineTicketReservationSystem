9eea3f3bb2357244d678aa10f321e995d8884215
stepin104986 <69206637+stepin104986@users.noreply.github.com>
2020-10-06 13:49:11 +0530

    Update airline_operations.h

--- 03_Implementation/inc/airline_operations.h
+++ 03_Implementation/inc/airline_operations.h
@@ -6,12 +6,12 @@
 #include<stdlib.h>
 #include<string.h>
 
-/*void reservation(void);							//main reservation function
+void reservation(void);							//main reservation function
 void viewdetails(void);							//view details of all the flight
 void printticket(char name[],int,char class_pref[],int,float);	//print ticket
 void specificflight(int);						//print data related to specific flight
 float charge(int,int);							//charge calculated automatically w.r.t number of seats and flight
-void login();*/
-int userLogin(char uname[10],char pword[10]);
+void login();
+int userLogin (char uname[10],char pword[10]);
 
 #endif                                              // __AIRLINE_OPERATIONS_H__
