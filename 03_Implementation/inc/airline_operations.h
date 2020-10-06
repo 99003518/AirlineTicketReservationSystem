1081f4256daff4144514b2ea2738de04d26c5f68
stepin104986 <69206637+stepin104986@users.noreply.github.com>
2020-10-06 13:31:52 +0530

    Update airline_operations.h

--- 03_Implementation/inc/airline_operations.h
+++ 03_Implementation/inc/airline_operations.h
@@ -2,15 +2,16 @@
 #define __AIRLINE_OPERATIONS_H__
 
 #include<stdio.h>
-#include<conio.h>
+//#include<conio.h>
 #include<stdlib.h>
 #include<string.h>
 
-void reservation(void);							//main reservation function
+/*void reservation(void);							//main reservation function
 void viewdetails(void);							//view details of all the flight
 void printticket(char name[],int,char class_pref[],int,float);	//print ticket
 void specificflight(int);						//print data related to specific flight
 float charge(int,int);							//charge calculated automatically w.r.t number of seats and flight
-void login();
+void login();*/
+int userLogin(char uname[10],char pword[10]);
 
 #endif                                              // __AIRLINE_OPERATIONS_H__
