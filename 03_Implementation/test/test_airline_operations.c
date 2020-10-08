d23bfbbce4b28bf70a36323231f9ab168690962c
stepin104986 <69206637+stepin104986@users.noreply.github.com>
2020-10-08 13:00:50 +0530

    Update test_airline_operations.c

--- 03_Implementation/test/test_airline_operations.c
+++ 03_Implementation/test/test_airline_operations.c
@@ -4,7 +4,7 @@
 
 /* Modify these two lines according to the project */
 #include <airline_operations.h>
-#define PROJECT_NAME    "Airline"
+#define PROJECT_NAME    "AIRLINE"
 
 /* Prototypes for all the test functions */
 void test_userLogin (void);
